e66f50b5aedf9078a8b89b5e09d44146c60ef92a
Jérémy <jeremy.delpech.martin@gmail.com>
2025-07-21 21:22:10 +0200

    Init db/
    Signed-off-by: Jérémy <jeremy.delpech.martin@gmail.com>

---
+++ app/db/base.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import DeclarativeBase
+
+
+# Base for Alembic -> it need it to generate all migrations
+class Base(DeclarativeBase):
+    pass
---
+++ app/db/session.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+# Build engine
+engine = create_engine(settings.DATABASE_URL, echo=True, future=True)
+
+# Session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+
+
+# Dependance FastAPI
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
