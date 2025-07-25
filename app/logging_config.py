84ff8feae09eb00080551a64df50cb92bd97ecf5
Jérémy <jeremy.delpech.martin@gmail.com>
2025-07-25 22:19:32 +0200

    Add: global logging
    Signed-off-by: Jérémy <jeremy.delpech.martin@gmail.com>

---
+++ app/logging_config.py
@@ -0,0 +1,21 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+# logs directory
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = os.path.join(LOG_DIR, "server.log")
+
+# config
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        RotatingFileHandler(LOG_FILE, maxBytes=10_000_000, backupCount=3),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("mindquest")
